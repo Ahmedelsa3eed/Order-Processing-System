fc0e1165941c84da01ede0133306c4f2fb582ac4
Ahmedelsa3eed <73740339+Ahmedelsa3eed@users.noreply.github.com>
2023-01-02 01:19:14 +0200

    first commit

---
+++ Order-Processing-Backend/src/main/java/csed/database/orderprocessingbackend/OrderProcessingBackendApplication.java
@@ -0,0 +1,13 @@
+package csed.database.orderprocessingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProcessingBackendApplication.class, args);
+    }
+
+}
---
+++ Order-Processing-Backend/src/test/java/csed/database/orderprocessingbackend/OrderProcessingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package csed.database.orderprocessingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
