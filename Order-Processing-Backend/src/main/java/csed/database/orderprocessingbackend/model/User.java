11bf11d3f592828f858452d393fc6d94b87ed342
mo-ayman <m.ayman.saaed@gmail.com>
2023-01-03 00:13:21 +0200

    Add sessions services

--- Order-Processing-Backend/src/main/java/csed/database/orderprocessingbackend/model/User.java
+++ Order-Processing-Backend/src/main/java/csed/database/orderprocessingbackend/model/User.java
@@ -2,7 +2,6 @@ package csed.database.orderprocessingbackend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -10,15 +9,15 @@ import java.io.Serializable;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class User implements Serializable {
-    private Long user_id;
+    private String user_id;
     private String user_name;
-    private String first_name;
-    private String last_name;
-    private String address;
-    private String phonenumber;
     private String email;
     private String password;
+    private String address;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
     private String type;
+
 }
