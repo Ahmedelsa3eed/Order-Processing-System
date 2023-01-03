939a3a7a9d86b8a8931298689f906563b060e9c3
Ahmedelsa3eed <73740339+Ahmedelsa3eed@users.noreply.github.com>
2023-01-04 00:38:12 +0200

    integrate accounts with backend

--- Order-Processing-Backend/src/main/java/csed/database/orderprocessingbackend/model/User.java
+++ Order-Processing-Backend/src/main/java/csed/database/orderprocessingbackend/model/User.java
@@ -2,12 +2,14 @@ package csed.database.orderprocessingbackend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
     private Long user_id;
     private String user_name;
